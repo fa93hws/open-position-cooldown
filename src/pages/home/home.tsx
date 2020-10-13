ce0cd58d10f6e5a7da86fd75c53668e28679385c
Eric Wang <wjun0912@gmail.com>
2020-10-13 14:33:02 +1100

    feat: remove all css files (#22)

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,4 +1,3 @@
 import * as React from 'react';
-import styles from './home.css';
 
-export const HomePage = () => <div className={styles.home}>home!</div>;
+export const HomePage = () => <div>home!</div>;
